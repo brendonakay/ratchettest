0856f875dad5c9555ed964bcc78942de2527f0d2
Brendon Kay <bak04280@gmail.com>
2019-02-25 21:10:07 -0700

    wip

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ Tutorial: http://rkulla.blogspot.com/2016/01/data-pipeline-and-etl-tasks-in-go-u
 TODO:
 	- Refactor mysql input & processors reader to csv
 		- Rewrite setupDB to setupCSV
+		- ** Rewrite SQL reader to FileReader
 */
 package main
 
@@ -18,15 +19,19 @@ import (
 
 // I have no idea what I'm doing :]
 func main() {
-	inputDB := setupDB("mysql", "root:@tcp(127.0.0.1:3306)/srcDB")
-	extractDP := processors.NewSQLReader(inputDB, mypkg.Query(5))
+	// inputDB := setupDB("mysql", "root:@tcp(127.0.0.1:3306)/srcDB")
+	// extractDP := processors.NewSQLReader(inputDB, mypkg.Query(5)) //TODO: rewrite to extractCSV
+	extractCSV := processors.NewFileReader(inputCSV)
 
+	// TODO: Rewrite to CSVTransformaer
  	transformDP := mypkg.NewMyTransformer()
 
+	// TODO: Do the same as above with writing out to CSV
 	outputDB := setupDB("mysql", "root@tcp(127.0.0.1:3306)/dstDB")
 	outputTable := "users2"
 	loadDP := processors.NewSQLWriter(outputDB, outputTable)
 
+	// TODO: refactior da pipelane
 	pipeline := ratchet.NewPipeline(extractDP, transformDP, loadDP)
 	pipeline.Name = "My Pipeline"
 
@@ -40,7 +45,7 @@ func main() {
 }
 
 // Open the CSV file. CSV file must be in same directory as program.
-func setupCSV(driver, conn string) *sql.DB {
+func setupCSV(driver, conn string) *FileReader {
 	csvFile, err := os.Open("people.csv")
 	if err != nil {
 		logger.Error(err)
